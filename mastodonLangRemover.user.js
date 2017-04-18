243c0ea10787cb9d16e0039b4035f811fbcb518b
Bernard McKeever <dregin@gmail.com>
2017-04-19 00:40:09 +0100

    debug code

--- mastodonLangRemover.user.js
+++ mastodonLangRemover.user.js
@@ -29,7 +29,10 @@
 				var regex = /[\u3000-\u303F]|[\u3040-\u309F]|[\u30A0-\u30FF]|[\uFF00-\uFFEF]|[\u4E00-\u9FAF]|[\u2605-\u2606]|[\u2190-\u2195]|\u203B/g; 
 				if(regex.test(text)) {
 					toot.remove();
-				}
+          console.log("RM--" + toot.children('.status__content').text());
+				} else {
+          console.log(langText + '==' + toot.children('.status__content').text());
+        }
         return String(GM_getValue('lang', 'ja'));
     }
 
