dedfa1aa9c2a23d6da5078b37fe9a6189a4d3a8e
Bernard McKeever <dregin@gmail.com>
2017-04-27 21:19:56 +0100

    hide() instead of remove() to keep ReactJS happy
    removing DOM elements was causing React to assplode.

--- mastodonLangRemover.user.js
+++ mastodonLangRemover.user.js
@@ -27,7 +27,7 @@
         var text = toot.children('.status__content').text().replace(/(?:https?|ftp):\/\/[\n\S]+/g, '');
 				var regex = /[\u3000-\u303F]|[\u3040-\u309F]|[\u30A0-\u30FF]|[\uFF00-\uFFEF]|[\u4E00-\u9FAF]|[\u2605-\u2606]|[\u2190-\u2195]|\u203B/g;
 				if(regex.test(text)) {
-					toot.remove();
+					toot.hide();
         }
         return String(GM_getValue('lang', 'ja'));
     }
